ad9b9cc7318ae0880251856e623e23780cd90789
Maksym Archybasov <maksym.archybasov@3shape.com>
2020-08-10 22:30:46 +0300

    test task for dynamicsEducation

---
+++ CSharpTest.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpTest.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpTest.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f5c8edd-0ced-4326-9a90-798340f1291a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            HashSet<DateTime> daysOff = default;
+            if (weekEnds != null && weekEnds.Length > 0)
+            {
+                daysOff = GetDaysOffDates(weekEnds);
+            }
+            DateTime tempDate = startDate;
+            for (int i = 1; i < dayCount; i++)
+            {
+                if (daysOff != default && daysOff.Contains(tempDate))
+                {
+                    dayCount++;
+                }
+                tempDate = IncrementDate(tempDate);
+            }
+
+            return tempDate;
+        }
+        private HashSet<DateTime> GetDaysOffDates(WeekEnd[] weekEnds)
+        {
+            HashSet<DateTime> daysOff = new HashSet<DateTime>();
+            foreach (var date in weekEnds)
+            {
+                DateTime tempDate = date.StartDate;
+                do
+                {
+                    if (!daysOff.Contains(tempDate))
+                    {
+                        daysOff.Add(tempDate);
+                    }
+                    tempDate = tempDate.AddDays(1);
+                }
+                while (tempDate <= date.EndDate);
+            }
+            return daysOff;
+        }
+        private DateTime IncrementDate(DateTime date) => date.AddDays(1);
+    }
+}
